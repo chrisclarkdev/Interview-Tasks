de1fb439a2a2f5dfab2e6e15d76a6fc98200788e
Chrisclrk9 <chrisclrk9@gmail.com>
2020-05-04 19:34:54 +0100

    cleaned up the code

--- FizzBuzz/app.js
+++ FizzBuzz/app.js
@@ -1,14 +1,10 @@
 let newNum;
 function fizzbuzz() {
   for( num = 1; num <= 100; num++ ){
-  
     let newNum = (( num % 15  === 0  ? "fizzbuzz" : num  % 5 == 0 ? "buzz" : num % 3 === 0 ? "fizz" :num ))
-    // console.log(newNum)
-    const app = document.querySelector('#app');
-   app.innerHTML = newNum;
-   console.log(newNum)
+    console.log(newNum);
   }
 }
 fizzbuzz();
 
-// console.log(newNum);
+   
